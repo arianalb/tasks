0311c2c98831c4c38373e2fe1f20affd29c80fc9
Ariana Butler <arianabutler@wifi-roaming-128-4-155-94.wifi.udel.edu>
2026-08-27 12:57:05 -0400

    added hello world and created check command

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Ariana Butler
+                UD CISC275 with React Hooks and TypeScript Ariana Butler Hello
+                World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Hello world
             </p>
         </div>
     );
